53b0f63593106737cdc66e7c26143c333a259b9a
LuongDang <luong.dang@saritasa.com>
2018-08-29 08:58:58 +0700

    Create basic structure of vue app, webpack

---
+++ src/components/search/SearchResult.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search-result-wrapper">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Search Result'
+    }
+  }
+}
+</script>
---
+++ src/components/tag-cloud/TagCloud.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="tag-cloud-wrapper">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Tag Cloud'
+    }
+  }
+}
+</script>
