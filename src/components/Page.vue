703f5f9f28ecb2d0b9297f8b23d236cdc6f7e6bc
LuongDang <luong.dang@saritasa.com>
2018-09-27 08:27:05 +0700

    PHPR-7 Add 'explain' counting

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -5,209 +5,242 @@
     </div>
 
     <div class="section tag-cloud">
-      <tag-cloud ref="tagCloud" :onWordClick="onWordClick"></tag-cloud>
+      <tag-cloud ref="tagCloud"
+                 :on-word-click="onWordClick"/>
     </div>
 
     <div class="section breadcrumb">
-      <span class="selected-tag" v-bind:key="index" v-for="(selectedTag, index) in selectedTags">
-        {{ selectedTag }}<span class="icon" @click="clearTag(index)"><v-icon small>close</v-icon></span>
+      <span v-for="(selectedTag, index) in selectedTags"
+            :key="index"
+            class="selected-tag">
+        {{ selectedTag }}<span class="icon"
+                               @click="clearTag(index)"><v-icon small>close</v-icon></span>
       </span>
     </div>
 
     <div class="section search-box">
-      <search-box ref="searchBox" :searchChange="searchChange"></search-box>
+      <search-box ref="searchBox"
+                  :search-change="searchChange"/>
     </div>
 
     <div class="result-count">
-      <span>{{ this.resultCount }} results found.</span>
+      <span>{{ resultCount }} results found.</span>
+      <span v-if="explainCount > 0">({{ explainCount }} matched in 'explain' content)</span>
     </div>
     <div class="section resource">
       <resource-sections
-        v-for="(item, name) in this.resources"
-        v-bind:key="name"
-        :name="name"
-        :resource="item"
-      >
-      </resource-sections>
+          v-for="(item, name) in resources"
+          :key="name"
+          :name="name"
+          :resource="item"
+      />
+    </div>
+
+    <div class="section footer">
+      <quick-link></quick-link>
     </div>
   </div>
 </template>
 
 <script>
-import TagCloud from '@/components/tag-cloud/TagCloud.vue'
-import SearchBox from '@/components/search/SearchBox.vue'
-import ResourceSections from '@/components/ResourceSections.vue'
-import Yaml from 'yamljs'
-import _ from 'lodash'
+  import TagCloud from '@/components/tag-cloud/TagCloud';
+  import SearchBox from '@/components/search/SearchBox';
+  import ResourceSections from '@/components/ResourceSections';
+  import QuickLink from '@/components/footer/QuickLink';
+  import Yaml from 'yamljs';
+  import _ from 'lodash';
 
-export default {
-  components: {
-    TagCloud,
-    SearchBox,
-    ResourceSections
-  },
-  data () {
-    return {
-      yamlData: Yaml.load('/static/list.yaml'),
-      resources: {},
-      resultCount: 0,
-      selectedTags: []
-    }
-  },
-  methods: {
-    onWordClick (clickedWord, vm) {
-      if (_.indexOf(this.selectedTags, clickedWord) === -1) {
-        this.selectedTags.push(clickedWord)
-        this.$refs.tagCloud.selectedTags = this.selectedTags
-        this.searchChange(window.event, this.$refs.searchBox.keyword)
-      }
+  export default {
+    components: {
+      TagCloud,
+      SearchBox,
+      ResourceSections,
+      QuickLink,
     },
-    /**
-     * Get array of words in a phrase. Not include special chars.
-     * A word can be group of alphabets or group of digits.
-     * Each words will became lowercase word.
-     *
-     * @param phrase A string
-     */
-    pureWords (phrase) {
-      if (phrase === undefined) {
-        return phrase
-      }
-      if (_.isArray(phrase)) {
-        phrase = _.join(_.flattenDeep(phrase), ' ')
-      }
+    data() {
+      return {
+        yamlData:     Yaml.load('/static/list.yaml'),
+        resources:    {},
+        resultCount:  0,
+        explainCount: 0,
+        selectedTags: [],
+      };
+    },
+    created() {
+      // Add 'url' to website item if it hasn't
+      _.forEach(this.yamlData, (val, k) => {
+        _.forEach(val.websites, (item, key) => {
+          let url = item.url || key;
 
-      return _.words(phrase.toLowerCase(), /[-\w]+/g)
-    },
-    toLowerTags (tags) {
-      _.forEach(tags, (tag, key) => {
-        tags[key] = tag.toLowerCase()
-      })
+          if (!_.startsWith(url, 'http')) {
+            url = `http://${url}`;
+          }
+          this.yamlData[k].websites[key].url = url;
+        });
+      });
 
-      return tags
+      this.searchChange();
     },
-    /**
-     * Split text to array of words and marks (, . @ / so on).
-     * E.x: The text "quick, brown @fox" will be split to ['quick', ',', ' ', 'brown', ' ', '@', 'fox']
-     *
-     * @param text
-     */
-    fragString (text) {
-      return text.split(/([^a-zA-Z])/g)
-    },
-    highlight (keywords, sentence) {
-      keywords = this.pureWords(keywords)
-
-      let sentenceWords = this.fragString(sentence)
-      _.forEach(sentenceWords, (val, key) => {
-        if (_.indexOf(keywords, val.toLowerCase()) !== -1) {
-          sentenceWords[key] = '<span class="highlighted-word">' + val + '</span>'
+    methods: {
+      onWordClick(clickedWord) {
+        if (_.indexOf(this.selectedTags, clickedWord) === -1) {
+          this.selectedTags.push(clickedWord);
+          this.$refs.tagCloud.selectedTags = this.selectedTags;
+          this.searchChange(window.event, this.$refs.searchBox.keyword);
         }
-      })
+      },
+      /**
+       * Get array of words in a phrase. Not include special chars.
+       * A word can be group of alphabets or group of digits.
+       * Each words will became lowercase word.
+       *
+       * @param phrase A string
+       */
+      pureWords(phrase) {
+        if (phrase === undefined) {
+          return phrase;
+        }
 
-      return _.join(sentenceWords, '')
-    },
-    searchChange (event, keyword) {
-      keyword = keyword === undefined ? '' : keyword
-      let val = _.trim(keyword)
-      let words = this.pureWords(val)
+        let result = phrase;
 
-      this.resultCount = 0
+        if (_.isArray(phrase)) {
+          result = _.join(_.flattenDeep(phrase), ' ');
+        }
 
-      let filteredResources = {}
-      _.forEach(this.yamlData, (resource, resourceKey) => {
-        //
-        let filteredLists = {}
-        _.forEach(resource, (list, listKey) => {
-          // listKey = 'packages', 'websites'...
-          let filteredItems = {}
-          _.forEach(list, (item, itemKey) => {
-            //
-            let matched = false
-            let matchedItem = _.clone(item)
+        return _.words(result.toLowerCase(), /[-\w]+/g);
+      },
+      toLowerTags(tags) {
+        const result = [];
 
-            // 1. Filter by selected tags in cloud
-            let noTag = false
-            if (this.selectedTags.length > 0) {
-              _.forEach(this.selectedTags, (selectedTag) => {
-                if (_.indexOf(this.toLowerTags(item['tags']), selectedTag.toLowerCase()) === -1) {
-                  noTag = true
-                }
-              })
-            }
-            if (noTag === true) {
-              return
-            }
+        _.forEach(tags, (tag, key) => {
+          result[key] = tag.toLowerCase();
+        });
 
-            // 2. Filter by keywords
-            if (words.length === 0) {
-              matched = true
-            } else {
-              // 2.1 Check with article key. E.x: 'saritasa/common', 'dingo/api'
-              if (_.intersection(words, this.pureWords(itemKey)).length > 0) {
-                itemKey = this.highlight(words, itemKey)
-                matched = true
+        return result;
+      },
+      /**
+       * Split text to array of words and marks (, . @ / so on).
+       * E.x: The text "quick, brown @fox" will be split to ['quick', ',', ' ', 'brown', ' ', '@', 'fox']
+       *
+       * @param text
+       */
+      fragString(text) {
+        return text.split(/([^a-zA-Z])/g);
+      },
+      highlight(keywords, sentence) {
+        const pureKeywords = this.pureWords(keywords);
+
+        const sentenceWords = this.fragString(sentence);
+
+        _.forEach(sentenceWords, (val, key) => {
+          if (_.indexOf(pureKeywords, val.toLowerCase()) !== -1) {
+            sentenceWords[key] = `<span class="highlighted-word">${val}</span>`;
+          }
+        });
+
+        return _.join(sentenceWords, '');
+      },
+      searchChange(event, kw) {
+        const keyword = kw === undefined ? '' : kw;
+
+        const val = _.trim(keyword);
+        const words = this.pureWords(val);
+
+        this.resultCount = 0;
+        this.explainCount = 0;
+
+        const filteredResources = {};
+
+        _.forEach(this.yamlData, (resource, resourceKey) => {
+          //
+          const filteredLists = {};
+
+          _.forEach(resource, (list, listKey) => {
+            // listKey = 'packages', 'websites'...
+            const filteredItems = {};
+
+            _.forEach(list, (item, itemKey) => {
+              let highlightKey = itemKey;
+              //
+              let matched = false;
+              const matchedItem = _.clone(item);
+
+              // 1. Filter by selected tags in cloud
+              let noTag = false;
+
+              if (this.selectedTags.length > 0) {
+                _.forEach(this.selectedTags, selectedTag => {
+                  if (_.indexOf(this.toLowerTags(item.tags), selectedTag.toLowerCase()) === -1) {
+                    noTag = true;
+                  }
+                });
               }
 
-              // 2.2 Check with article content
-              _.forEach(item, (text, label) => {
-                if (_.intersection(words, this.pureWords(text)).length > 0) {
-                  // Except 'explain' and 'url' because they aren't shown as text
-                  if (label !== 'explain' && label !== 'url' && label !== 'tags') {
-                    matchedItem[label] = this.highlight(words, text)
-                  } else {
-                    matchedItem[label] = text
-                  }
-                  matched = true
+              if (noTag === true) {
+                return;
+              }
+
+              // 2. Filter by keywords
+              if (words.length === 0) {
+                matched = true;
+              } else {
+                // 2.1 Check with article key. E.x: 'saritasa/common', 'dingo/api'
+                if (_.intersection(words, this.pureWords(itemKey)).length > 0) {
+                  highlightKey = this.highlight(words, itemKey);
+                  matched = true;
                 }
-              })
+
+                // 2.2 Check with article content
+                _.forEach(item, (text, label) => {
+                  if (_.intersection(words, this.pureWords(text)).length > 0) {
+                    // Except 'explain' and 'url' because they aren't shown as text
+                    if (label !== 'explain' && label !== 'url' && label !== 'tags') {
+                      matchedItem[label] = this.highlight(words, text);
+                    } else {
+                      matchedItem[label] = text;
+                      if (label === 'explain') {
+                        this.explainCount++;
+                      }
+                    }
+                    matched = true;
+                  }
+                });
+              }
+
+              if (matched === true) {
+                filteredItems[highlightKey] = matchedItem;
+                this.resultCount++;
+              }
+            });
+            if (_.size(filteredItems) > 0) {
+              filteredLists[listKey] = filteredItems;
             }
-            if (matched === true) {
-              filteredItems[itemKey] = matchedItem
-              this.resultCount++
-            }
-          })
-          if (_.size(filteredItems) > 0) {
-            filteredLists[listKey] = filteredItems
+          });
+          if (_.size(filteredLists) > 0) {
+            filteredResources[resourceKey] = filteredLists;
           }
-        })
-        if (_.size(filteredLists) > 0) {
-          filteredResources[resourceKey] = filteredLists
+        });
+
+        this.resources = '';
+        Object.keys(this.resources).forEach(prop => {
+          delete this.resources[prop];
+          this.resources[prop] = undefined;
+        });
+
+        this.resources = filteredResources;
+        if (this.$refs.tagCloud !== undefined) {
+          // TagCloud was loaded
+          this.$refs.tagCloud.searchResult = filteredResources;
         }
-      })
 
-      this.resources = ''
-      Object.keys(this.resources).forEach(function (prop) {
-        delete this.resources[prop]
-        this.resources[prop] = undefined
-      })
-
-      this.resources = filteredResources
-      if (this.$refs.tagCloud !== undefined) {
-        // TagCloud was loaded
-        this.$refs.tagCloud.searchResult = filteredResources
-      }
-      return false
+        return false;
+      },
+      clearTag(tagIndex) {
+        this.$delete(this.selectedTags, tagIndex);
+        this.searchChange(window.event, this.$refs.searchBox.keyword);
+      },
     },
-    clearTag (tagIndex) {
-      this.$delete(this.selectedTags, tagIndex)
-      this.searchChange(window.event, this.$refs.searchBox.keyword)
-    }
-  },
-  created () {
-    // Add 'url' to website item if it hasn't
-    _.forEach(this.yamlData, (val) => {
-      _.forEach(val['websites'], (item, key) => {
-        val['websites'][key]['url'] = item.url || key
-        if (!_.startsWith(val['websites'][key]['url'], 'http')) {
-          val['websites'][key]['url'] = 'http://' + val['websites'][key]['url']
-        }
-      })
-    })
-
-    this.searchChange()
-  }
-}
+  };
 </script>
 
 <style lang="scss">
@@ -225,7 +258,9 @@ export default {
       white-space: nowrap;
       background-color: #27a927;
       color: #fff;
-      line-height: 30px;
+      line-height: 22px;
+      height: 22px;
+      display: inline-block;
       &:before {
         content:"";
         position: absolute;
---
+++ src/components/footer/QuickLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="quick-link-wrapper">
+    <span>©Since 09-2018</span>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  .quick-link-wrapper {
+    margin: 0 auto;
+    padding: 20px 0;
+    text-align: center;
+  }
+</style>
