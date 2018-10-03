da6b8cef9c2eaaf70f9409428e6a4ddc1c954f7d
Sergey Populov <sergey@saritasa.com>
2018-10-03 16:28:25 +0500

    Fix local dev server

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -24,11 +24,11 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
-        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
+        // { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
       ],
     },
     hot: true,
-    contentBase: false, // since we use CopyWebpackPlugin.
+    contentBase: baseWebpackConfig.output.path,
     compress: true,
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
