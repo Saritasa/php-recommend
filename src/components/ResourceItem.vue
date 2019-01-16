9b76c449a87735bc70d125bb16321c2fa29168a9
Sergey Populov <sergey@saritasa.com>
2019-01-16 21:38:38 +0500

    Use markdown

--- src/components/ResourceItem.vue
+++ src/components/ResourceItem.vue
@@ -3,20 +3,27 @@
     <a :href="item.url">
       <span v-html="item.name"></span>
     </a>
-    <span v-if="item.desc">- <span v-html="item.desc"/></span>
-    <v-tooltip bottom v-if="item.getExplanation()">
+    <span v-if="item.desc">- <span v-html="markdown(item.desc)"/></span>
+    <v-tooltip bottom close-delay="600" v-if="item.explanation">
       <v-icon slot="activator" small>info</v-icon>
-      {{ item.explanation }}
+      <span v-html="markdown(item.explanation)"></span>
     </v-tooltip>
   </li>
 </template>
 
 <script>
+import marked from 'marked';
+
 export default {
   props: {
     item: {
       type: Object,
       default: null,
+    },
+  },
+  methods: {
+    markdown(value) {
+      return marked.inlineLexer(value, []);
     },
   },
 };
